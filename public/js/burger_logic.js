023668d62424c5d8b3648959f696054ccee28f6b
Renzo Renteria <Renzo@Renzos-MacBook-Pro.local>
2017-10-02 09:22:41 -0400

    update

--- public/js/burger_logic.js
+++ public/js/burger_logic.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
 	var burgerInput = $("#text_area");
-	var burgerList = $("tbody");
+	var burgerList = $("#burger_body");
+	var newTr;
+
 
 	$(document).on("submit", "#burger_form", handleBurgerFormSubmit);
 	$(document).on("click", ".devour_btn", handleDevourBtn);
@@ -22,15 +24,12 @@ $(document).ready(function() {
 			.then(getBurgers);
 	}
 
-
-
 	function createBurgerRow(burgerData) {
 
 
-		var newTr = $("<tr>").attr('id', burgerData.id);
-		newTr.append("<td>" + burgerData.burger_name + "</td>");
-		newTr.append("<button type='button' class='devour_btn' value='false'>Devour</button>");
-		newTr.append("<td class='devoured_container'></td>");
+		newTr = $("<tr id='burger-form-row'>").attr('id', burgerData.id);
+		newTr.append("<td class='burger_item'>" + burgerData.burger_name + "</td>");
+		newTr.append("<input type='button' class='devour_btn' value='Devour' name='devour'></input>");
 		return newTr;
 	}
 
@@ -56,7 +55,6 @@ $(document).ready(function() {
 	function handleDevourBtn(event) {
 		event.preventDefault();
 
-		if ($(event.target).val() == 'false') {
 
 			$.ajax({
 					method: "PUT",
@@ -64,14 +62,19 @@ $(document).ready(function() {
 					data: {
 						id: $(event.target).parent().attr('id'),
 						devoured: true
-			  		}
-			    }).done(getBurgers);
+					},
+					success: function() {
+						var parent = $(event.target).parent();
+						var newList = parent[0].innerText;
 
-			
+						console.log(newList);
 
-		}
+						var newDevTr = $("<tr>")
+						newDevTr.append('<td>' + newList + '</td>');
+						newDevTr.appendTo('#devoured_body');
+						parent.remove();
+					}
+				});
 	}
-
-
 
 });
